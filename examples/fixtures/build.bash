6e4b14318b27e45bb3c0218552a37180be981a1c
Kamil Trzcinski <ayufan@ayufan.eu>
2017-03-28 14:56:46 +0200

    Add example scripts

---
+++ examples/fixtures/build.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+REPO=192.168.65.1:5000
+
+build_and_push() {
+    date > file2
+    docker build -t "$REPO/$1" .
+    docker push "$REPO/$1"
+}
+
+tag_and_push() {
+    docker tag "$REPO/$1" "$REPO/$2"
+    docker push "$REPO/$2"
+}
+
+build_and_push "test:latest"
+build_and_push "test:latest"
+tag_and_push "test:latest" "test2:latest"
+build_and_push "test:latest"
+tag_and_push "test:latest" "test2:latest2"
